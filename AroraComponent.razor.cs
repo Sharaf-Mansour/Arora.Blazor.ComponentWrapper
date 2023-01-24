9ee5e24b4eda673ba523d43a33c6ed2e92b65a63
Sharaf-Mansour <sharaf_1015@yahoo.com>
2023-01-24 22:07:09 +0200

    V1.0.2

--- AroraComponent.razor.cs
+++ AroraComponent.razor.cs
@@ -1,19 +1,25 @@
 using Microsoft.AspNetCore.Components;
-using System.Reflection;
 namespace Arora.Blazor.ComponentWrapper;
 public partial class AroraComponent
 {
     [Parameter]
     public required string ElementName { get; set; }
-
     RenderFragment? RenderChild;
     protected override void OnInitialized()
     {
         RenderChild = new(builder =>
         {
-            Type classType = Assembly.GetCallingAssembly().GetTypes().FirstOrDefault(t => t.FullName!.ToLower().Contains(ElementName.ToLower())) ?? throw new NullReferenceException("Wrong Name");
-            builder.OpenComponent(0, classType);
-            builder.CloseComponent();
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                var classType = assembly.GetTypes().FirstOrDefault(t => t.FullName!.ToLower().Contains(ElementName.ToLower()));
+                if (classType is not null)
+                {
+                    builder.OpenComponent(0, classType);
+                    builder.CloseComponent();
+                    return;
+                }
+            }
         });
     }
 }
